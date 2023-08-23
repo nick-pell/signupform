cd8e9546189a41a5778eb4a481354703d15f68c3
nick-pell <93624627+nick-pell@users.noreply.github.com>
2023-08-22 21:07:24 -0400

    Fix
    Linked submit button to inputs

---
+++ script.js
@@ -0,0 +1,8 @@
+const first_name = document.getElementById('first_name');
+const last_name = document.getElementById('last_name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const phone_number = document.getElementById('phone_number');
+const confirm_password = document.getElementById('confirm_password');
+const form = document.getElementById('form');
+
